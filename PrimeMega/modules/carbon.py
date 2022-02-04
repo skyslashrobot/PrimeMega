bfc461a769afc7bb11e189749c36d519f0ab79c3
skyslashrobot <98939956+skyslashrobot@users.noreply.github.com>
2022-02-05 02:38:00 +0700

    Update carbon.py

--- PrimeMega/modules/carbon.py
+++ PrimeMega/modules/carbon.py
@@ -27,8 +27,8 @@ async def carbon_func(_, message):
 @pbot.on_message(filters.command("repo"))
 async def repo(_, message):
     await message.reply_text(
-        f"""✨ **Hey I'm Prime Mega** 
-**Owner repo : [『TØNIC』](https://t.me/Bukan_guudlooking)**
+        f"""✨ **Hey I'm Skyslash 夜** 
+**Owner repo : [『Sukma』](https://t.me/Sunsilx)**
 **Python Version :** `{y()}`
 **Library Version :** `{o}`
 **Telethon Version :** `{s}`
@@ -38,8 +38,8 @@ async def repo(_, message):
         reply_markup=InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton("Repo", url="https://github.com/Tonic990/PrimeMega"), 
-                    InlineKeyboardButton("Support", url="https://t.me/PrimeSupportGroup")
+                    InlineKeyboardButton("Repo", url="www.Xhamster.com"), 
+                    InlineKeyboardButton("Support", url="https://t.me/SkyslashSupport")
                 ]
             ]
         ),
