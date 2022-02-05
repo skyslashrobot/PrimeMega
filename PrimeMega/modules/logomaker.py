814647af3c94ea7ac6d4c3e2b9344b7d2db37649
skyslashrobot <98939956+skyslashrobot@users.noreply.github.com>
2022-02-06 03:00:56 +0700

    Update logomaker.py

--- PrimeMega/modules/logomaker.py
+++ PrimeMega/modules/logomaker.py
@@ -113,7 +113,7 @@ async def logo_gen(event):
         await tbot.send_file(
             event.chat_id,
             file=flnme,
-            caption="Logo by [ᴘʀɪᴍᴇ ᴍᴇɢᴀ](https://t.me/PrimeMegaBot)",
+            caption="Logo by [Skyslash 夜](https://t.me/SkyslashhRobot)",
             force_document=False,
         )
         os.remove(flnme)
