3531f765137326d98c0c0cb1e04691f3c7757011
skyslashrobot <98939956+skyslashrobot@users.noreply.github.com>
2022-02-22 13:25:05 +0700

    Update alive.py

--- PrimeMega/modules/alive.py
+++ PrimeMega/modules/alive.py
@@ -15,10 +15,10 @@ PHOTO = "https://telegra.ph/file/8f0e7c3d5a62cf6c07767.jpg"
 async def awake(event):
   PRIME = f"**Hi [{event.sender.first_name}](tg://user?id={event.sender.id}), I'm Skyslash 夜.** \n\n"
   PRIME += "⚪ **I'm Working Properly** \n\n"
-  PRIME += f"⚪ **My God : [Sukma](https://t.me/sunsilx)** \n\n"
+  PRIME += f"⚪ **My God : [Sukma](https://t.me/Sansanzt)** \n\n"
   PRIME += f"⚪ **Library Version :** `{telever}` \n\n"
   PRIME += f"⚪ **Telethon Version :** `{tlhver}` \n\n"
   PRIME += f"⚪ **Pyrogram Version :** `{pyrover}` \n\n"
   PRIME += "**Thanks For Adding Me Here ❤️**"
-  BUTTON = [[Button.url("ʜᴇʟᴘ​", "https://t.me/SkyslashhRobot?start=help"), Button.url("sᴜᴘᴘᴏʀᴛ​", "https://t.me/SkyslashSupport")]]
+  BUTTON = [[Button.url("ʜᴇʟᴘ​", "https://t.me/SkyslashRobot?start=help"), Button.url("sᴜᴘᴘᴏʀᴛ​", "https://t.me/SkyslashSupport")]]
   await tbot.send_file(event.chat_id, PHOTO, caption=PRIME,  buttons=BUTTON)
